4a45e065e064d626e5d6562c25cd19fa2d1be720
W2-80292 <sidhantkodgire@gmail.com>
2024-11-14 18:13:14 +0530

    Sign Up UI with email Splash screen

---
+++ SplashScreen/app/src/main/java/com/sidhant/splashscreen/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.sidhant.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent=Intent(this,SignUp::class.java)
+            startActivity(intent)
+            finish()//for not getting back on splash screen
+        },3000)
+
+    }
+}
